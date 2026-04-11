d417858709cc15f9e26d927f74924b410fe46a49
Krish Koria <krishkoria2004@gmail.com>
2026-04-11 12:52:40 +0530

    Add camera node and improve jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,41 +10,53 @@ extends CharacterBody2D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
+
 func is_moving(input: float) -> bool:
 	return input != 0
 
 
 func update_animation(input: float) -> void:
+	if not is_on_floor():
+		animation_player.play("jump")
+		return
+
 	if is_moving(input):
 		animation_player.play("run")
 		sprite_2d.scale.x = sign(input)
 	else:
 		animation_player.play("idle")
-		
-	if not is_on_floor():
-		animation_player.play("jump")
-	
+
 
 func apply_gravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y = move_toward(velocity.y, max_fall_speed, gravity * delta)
-		
-		
+
+
 func apply_horizontal_movement(delta: float, input: float) -> void:
 	if is_moving(input):
 		velocity.x = move_toward(velocity.x, input * max_speed, acceleration * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
 
-func apply_jump(jump: bool) -> void:
-	if jump and is_on_floor():
-		velocity.y = -jump_force
+
+func apply_jump(jump_pressed: bool, jump_released: bool) -> void:
+	if is_on_floor():
+		if jump_pressed:
+			velocity.y = -jump_force
+	else:
+		if jump_released and velocity.y < -jump_force / 2:
+			velocity.y = -jump_force / 2
+
 
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
+
 	var input = Input.get_axis("ui_left", "ui_right")
 	apply_horizontal_movement(delta, input)
-	var jump = Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_accept")
-	apply_jump(jump)
+
+	var jump_pressed = Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_accept")
+	var jump_released = Input.is_action_just_released("ui_up") or Input.is_action_just_released("ui_accept")
+	apply_jump(jump_pressed, jump_released)
+
 	update_animation(input)
 	move_and_slide()
